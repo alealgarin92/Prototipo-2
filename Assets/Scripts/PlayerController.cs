d17d4a4dbcf2141c9fedc69d4e475c389bf3537c
Alejandro Algarin <alejandro.algarin@hotmail.com>
2025-01-09 22:16:50 -0300

    Update 1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float horizontalInput;
+    [SerializeField] private float speed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+    }
+}
