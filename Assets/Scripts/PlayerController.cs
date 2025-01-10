fe6436bbc4d984a5862d6f791b90b3c4a7aeaf05
ale4287 <alejandro.algarin@hotmail.com>
2025-01-10 04:17:39 -0300

    Update 2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float horizontalInput;
     [SerializeField] private float speed;
+    [SerializeField] private float xRange = 10;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,6 +16,16 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
 
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
